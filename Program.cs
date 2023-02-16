5a0d3ea5cb866d7b0fa15bfa02b964f5d6ce93a9
Lisann <lisann.dahlen@web.de>
2023-02-16 14:35:16 +0100

    added checktestClass and program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GitCheckpoint1
+{
+	internal class Program
+	{
+		static void Main( string[] args )
+		{
+			Console.WriteLine( "This is the Git checkpoint task 1.1" );
+			CheckTest.TestMessage( "Task 2 checked" );
+		}
+	}
+}
